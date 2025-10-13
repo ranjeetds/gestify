4c272a56f69a1a2c3e5145c87d324bed758d6dfe
Ranjeet D <ranjeetrdhumal@gmail.com>
2025-10-14 01:30:51 +0530

    Initial Commit

---
+++ Gestify/GestifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestifyApp.swift
+//  Gestify
+//
+//  Created by admin on 14/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct GestifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GestifyTests/GestifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  GestifyTests.swift
+//  GestifyTests
+//
+//  Created by admin on 14/10/2025.
+//
+
+import Testing
+@testable import Gestify
+
+struct GestifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
